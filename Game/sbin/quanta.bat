40d53414e5ca85f16714aa2ab3acc462348dc04e
xiyoo0812 <xiyoo0812@163.com>
2026-01-07 00:07:10 +0800

    gate联调

--- Game/sbin/quanta.bat
+++ Game/sbin/quanta.bat
@@ -4,7 +4,7 @@ taskkill /f /im quanta.exe
 start "discover" quanta.exe config/discover.conf --index=1 --port=1
 start "router1" quanta.exe config/router.conf  --index=1 --port=1
 ::start "router2" quanta.exe config/router.conf  --index=2 --port=2
-::start "cache1"  quanta.exe config/cache.conf   --index=1 --port=1
+start "cache1"  quanta.exe config/cache.conf   --index=1 --port=1
 start "mongo"   quanta.exe config/mongo.conf   --index=1 --port=1
 ::start "redis"   quanta.exe config/redis.conf   --index=1 --port=1
 start "gate1"   quanta.exe config/gateway.conf --index=1 --port=1
